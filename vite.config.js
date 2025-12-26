724e81fc6978fe3e2b4533cf144366be68c2abdf
Yougal kumar <kyougal70@gmail.com>
2025-12-27 00:02:04 +0900

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,41 @@
+import { defineConfig } from 'vite'
+import postcss from './postcss.config.js'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  define: {
+    'process.env': process.env
+  },
+  server: {
+    port: 3000
+  },
+
+  css: {
+    postcss,
+  },
+  plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: /^~.+/,
+        replacement: (val) => {
+          return val.replace(/^~/, "");
+        },
+      },
+      {
+        find: 'fs',
+        replacement: 'memfs',
+      },
+    ],
+  },
+
+  build: {
+    outDir: './build',
+    chunkSizeWarningLimit: 3200,
+    commonjsOptions: {
+
+      transformMixedEsModules: true,
+    }
+  }
+})
